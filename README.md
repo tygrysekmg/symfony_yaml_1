84e4fee0bf89de2afe5614270cc1d863e296110b
root <root@grzegorz-php72.com>
2018-04-10 18:57:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# symfony_yaml_1
+
+follow for image s2.png
---
+++ src/Command/YamlGrzegorzCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Yaml\Yaml;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class YamlGrzegorzCommand extends Command
+{
+    protected static $defaultName = 'yaml:grzegorz';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $value = Yaml::parse("foo: bar");
+        print_r($value);
+    }
+}
